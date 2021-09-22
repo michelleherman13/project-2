6a7b01655e5dd84a3706045d7d2d59938294a7e2
Felecia Helms <85764497+fhelms8@users.noreply.github.com>
2021-09-21 19:35:31 -0500

    query complete

--- query.sql
+++ query.sql
@@ -1,13 +1,13 @@
 -- Query to check successful load
-SELECT * FROM happiness;
-
+SELECT * FROM happy_2015;
+SELECT * FROM happy_2016;
 SELECT * FROM population;
 
-DELETE FROM happiness;
 
-DELETE FROM population;
+
+
 -- Join tables on county_id
-SELECT happiness.country, happiness.family, happiness.economy, happiness.health, happiness.freedom, happiness.happiness_score, population.country, population.year_2015, population.year_2016
-FROM happiness
-INNER JOIN population
-ON happiness.country = population.country;
+SELECT happy_2015.country, happy_2015.family, happy_2015.economy, happy_2015.health, happy_2015.freedom, happy_2015.happiness_score, population.year_2015, population.year_2016
+FROM happy_2015
+INNER JOIN population ON happy_2015.country = population.country;
+
--- schema.sql
+++ schema.sql
@@ -1,18 +1,28 @@
-DROP TABLE IF EXISTS happiness;
+DROP TABLE IF EXISTS happy_2015;
+DROP TABLE IF EXISTS happy_2016;
 DROP TABLE IF EXISTS population;
 
 -- Create Two Tables
-CREATE TABLE happiness (
-  country INT PRIMARY KEY,
-  family REAL,
-  economy REAL,
-  health REAL,
-  freedom REAL,
-  happiness_score REAL
+CREATE TABLE happy_2015 (
+  Country varchar(40) PRIMARY KEY,
+  Family REAL,
+  Economy REAL,
+  Health REAL,
+  Freedom REAL,
+  Happiness_Score REAL
+);
+
+CREATE TABLE happy_2016 (
+  Country varchar(40) PRIMARY KEY,
+  Family REAL,
+  Economy REAL,
+  Health REAL,
+  Freedom REAL,
+  Happiness_Score REAL
 );
 
 CREATE TABLE population (
-  country INT PRIMARY KEY,
-  year_2015 INT,
-  year_2016 INT
+  Country varchar(100) PRIMARY KEY,
+  Year_2015 bigserial NOT NULL,
+  Year_2016 bigserial not null
 );
